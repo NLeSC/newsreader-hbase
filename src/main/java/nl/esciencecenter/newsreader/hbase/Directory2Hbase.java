f90e563c936c131f6445da9bcc5eea3f28eb0572
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-04-21 14:42:18 +0200

    Initial commit with documents stored in a compressed column and with autoflush turned off.

---
+++ src/main/java/nl/esciencecenter/newsreader/hbase/Directory2Hbase.java
@@ -0,0 +1,81 @@
+package nl.esciencecenter.newsreader.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.compress.Compression;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+
+public class Directory2Hbase {
+    private static String tableName = "documents";
+    private static String familyName = "naf";
+    private static String columnName = "annotated";
+    private static HTable table;
+
+    public static void main(String[] args) throws IOException {
+//        Path rootPath = Paths.get(args[0]);
+        Path rootPath = Paths.get("/home/stefanv/data/newsreader/2000-medium-docs-en/docs/output/");
+
+        initHBase();
+
+        walkDirectory(rootPath);
+
+        closeHBase();
+    }
+
+    private static void closeHBase() throws IOException {
+        table.close();
+    }
+
+    private static void walkDirectory(Path rootPath) throws IOException {
+        FileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                addDocument(file);
+                return FileVisitResult.CONTINUE;
+            }
+        };
+
+        Files.walkFileTree(rootPath, visitor);
+    }
+
+    private static void initHBase() throws IOException {
+        Configuration config = HBaseConfiguration.create();
+        HBaseAdmin admin = new HBaseAdmin(config);
+        if (!admin.tableExists(tableName)) {
+            System.out.println("Creating table");
+            HTableDescriptor tableDesc = new HTableDescriptor(tableName);
+            HColumnDescriptor columnDesc = new HColumnDescriptor(familyName.getBytes());
+            // to have compression, hadoop native shared libraries are required
+            // See http://hbase.apache.org/book.html#_compressor_configuration_installation_and_use
+            columnDesc.setCompressionType(Compression.Algorithm.LZ4);
+            tableDesc.addFamily(columnDesc);
+            admin.createTable(tableDesc);
+        }
+        table = new HTable(config, tableName);
+        table.setAutoFlush(false, false);
+    }
+
+    private static void addDocument(Path file) {
+        String filename = file.getFileName().toString().toLowerCase();
+        if (filename.endsWith(".naf")) {
+            System.out.println("Inserting = " + filename);
+            try {
+                byte[] content = Files.readAllBytes(file);
+                Put put = new Put(filename.getBytes());
+                put.add(familyName.getBytes(), columnName.getBytes(), content);
+                table.put(put);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/test/java/nl/esciencecenter/newsreader/hbase/Directory2HbaseTest.java
@@ -0,0 +1,13 @@
+package nl.esciencecenter.newsreader.hbase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Directory2HbaseTest {
+
+    @Test
+    public void testSomeThing()  {
+        assertEquals("1", "1");
+    }
+}
